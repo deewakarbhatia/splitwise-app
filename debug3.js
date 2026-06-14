68c166bcc3c42ce854bab34980356b6e2cf31b59
deewakarbhatia <deewakar.bhatia@gmail.com>
2026-06-14 16:16:21 +0530

    Remove debugging scripts

--- debug3.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const { parse } = require('csv-parse/sync');
-const recs = parse(fs.readFileSync('Expenses Export.csv', 'utf-8'), { columns: true, skip_empty_lines: true });
-
-recs.forEach((r, i) => {
-  if (r.split_type === 'share') {
-    console.log('ROW', i + 2, r);
-  }
-});
